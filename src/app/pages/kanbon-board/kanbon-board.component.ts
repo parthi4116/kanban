126e72042ad4e43976153f5822e04774bf827dbb
parthinanonino <parthi@nanonino.in>
2024-06-20 01:40:44 +0530

    drag drop

--- src/app/pages/kanbon-board/kanbon-board.component.ts
+++ src/app/pages/kanbon-board/kanbon-board.component.ts
@@ -1,11 +1,11 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-export interface itemNames{
+export interface itemNames {
   description: any,
-  done:boolean;
-  date:any,
-      title:any
+  done: boolean;
+  date: any,
+  title: any
 }
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -16,67 +16,91 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 
 export class KanbonBoardComponent implements OnInit {
-  itemsColumn1 :any= ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
-  itemsColumn2 :any= [];
-  itemsColumn3 :any= [];
-  editingIndex: { column: number, index: number } | null = null;
-  stockForm :FormGroup;
-taskList: any []=[];
-inprogress:any []=[];
-  done: any[] = [];
-updateIndex!:any;
-isEditEnabled:boolean=false;
-  constructor(private fb:FormBuilder, private modelservice:NgbModal) { }
+  stockForm: FormGroup;
+  todoList: any[] = [];
+  inprogress: any[] = [];
+  doneList: any[] = [];
+  testList: any[] = [];
+  deployList: any[] = [];
+  updateIndex!: any;
+  isEditEnabled: boolean = false;
+  checktype: any;
+  constructor(private fb: FormBuilder, private modelservice: NgbModal) { }
   @ViewChild('add', { static: false }) add: ElementRef;
 
   ngOnInit(): void {
-    this.stockForm=this.fb.group({
+    this.stockForm = this.fb.group({
       title: [' ', Validators.required],
       desc: [' ', Validators.required],
-     date: [' ', Validators.required]
+      date: [' ', Validators.required]
     })
   };
   //  this.modalService.open(this.popupChooseCard, { centered: true, size: 'md' });
-  opendia(){
+  opendia() {
     this.modelservice.open(this.add, { centered: true, size: 'md' });
   }
-  addList(){
-     this.taskList.push({
-      description:this.stockForm.value.desc,
-      date:this.stockForm.value.date,
-      title:this.stockForm.value.title,
-      done:false
-     })
+  addList() {
+    this.todoList.push({
+      description: this.stockForm.value.desc,
+      date: this.stockForm.value.date,
+      title: this.stockForm.value.title,
+      done: false
+    });
+    this.stockForm.reset()
+    this.modelservice.dismissAll()
   }
-  deleteItem(index:number){
-    this.taskList.splice(index,1);  
+  deleteItem(index: number,checktype:any) {
+     if (checktype== 'inprgress') {
+      this.inprogress.splice(index, 1);
+    } else if (checktype == 'stack') {
+      this.todoList.splice(index, 1)
+    }else if (checktype == 'done') {
+      this.doneList.splice(index, 1)
+    }else if (checktype== 'test') {
+      this.testList.splice(index, 1)
+    }else if (checktype == 'deploy') {
+      this.deployList.splice(index, 1)
+    }
   }
-  deleteInprogItem(index:number){
-     this.inprogress.splice(index,1)
-  }
-  deleteDoneItem(index:number){
-    this.done.splice(index,1)
-  }
-  onEdit(item:itemNames,index:number){
-    console.log(item);
-    console.log(this.inprogress);
-
-     this.stockForm.controls['title'].setValue(item.title)
-     this.updateIndex=index;
-     this.isEditEnabled=true;
+   
+  onEdit(item: itemNames, index: number, type: any) {
+    this.checktype = type
+    this.opendia();
+    this.stockForm.controls['title'].setValue(item.title)
+    this.stockForm.controls['date'].setValue(item.date)
+    this.stockForm.controls['desc'].setValue(item.description)
+    this.updateIndex = index;
+    this.isEditEnabled = true;
   };
-  onEdit1(item:itemNames,index:number){
-     this.stockForm.controls['title'].setValue(item.title)
-     this.updateIndex=index;
-     this.isEditEnabled=true;
-  }
-  updateList(type:any){
-    this.inprogress[this.updateIndex].title = this.stockForm.value.title;
-    this.taskList[this.updateIndex].title = this.stockForm.value.title;
+
+  updateList(type: any) {
+     if (this.checktype == 'inprgress') {
+      this.inprogress[this.updateIndex].title = this.stockForm.value.title;
+      this.inprogress[this.updateIndex].date = this.stockForm.value.date;
+      this.inprogress[this.updateIndex].description = this.stockForm.value.desc;
+    } else if (this.checktype == 'stack') {
+      this.todoList[this.updateIndex].title = this.stockForm.value.title;
+      this.todoList[this.updateIndex].date = this.stockForm.value.date;
+      this.todoList[this.updateIndex].description = this.stockForm.value.desc;
+    }else if (this.checktype == 'done') {
+      this.doneList[this.updateIndex].title = this.stockForm.value.title;
+      this.doneList[this.updateIndex].date = this.stockForm.value.date;
+      this.doneList[this.updateIndex].description = this.stockForm.value.desc;
+    }else if (this.checktype == 'test') {
+      this.testList[this.updateIndex].title = this.stockForm.value.title;
+      this.testList[this.updateIndex].date = this.stockForm.value.date;
+      this.testList[this.updateIndex].description = this.stockForm.value.desc;
+    }else if (this.checktype == 'deploy') {
+      this.deployList[this.updateIndex].title = this.stockForm.value.title;
+      this.deployList[this.updateIndex].date = this.stockForm.value.date;
+      this.deployList[this.updateIndex].description = this.stockForm.value.desc;
+    }
+    this.stockForm.reset()
+    this.modelservice.dismissAll()
   }
   drop(event: CdkDragDrop<itemNames[]>) {
     console.log(event);
-    
+
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -88,41 +112,6 @@ isEditEnabled:boolean=false;
       );
     }
   }
- 
 
-  drop1(event: CdkDragDrop<itemNames[]>) {
-    console.log(event);
-    
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
-    }
-  }
-
-  addItem() {
-    this.itemsColumn1.push(`Item ${this.itemsColumn1.length + 1}`);
-  }
-
-  editItem(column: number, index: number) {
-    this.editingIndex = { column, index };
-  }
-
-  saveItem(column: number, index: number, newValue: string) {
-    if (newValue.trim()) {
-      if (column === 1) {
-        this.itemsColumn1[index] = newValue;
-      } else if (column === 2) {
-        this.itemsColumn2[index] = newValue;
-      } else if (column === 3) {
-        this.itemsColumn3[index] = newValue;
-      }
-      this.editingIndex = null;
-    }
-  }
-
-  cancelEdit() {
-    this.editingIndex = null;
-  }
 
 }
