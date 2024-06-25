8f1a35c0e364ba0c0fa86cd17a7dcd43cc323b27
parthi <parthi@nanonino.in>
2024-06-25 10:56:21 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ToastrModule } from 'ngx-toastr';
+// import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +20,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot(),
+    // ToastrModule.forRoot(),
     MatCardModule,
     MatFormFieldModule,
     MatCardModule,
